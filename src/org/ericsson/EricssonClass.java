30ff133284c185163fa7e0a392e203eb4026b8f9
Saranesh1996 <saraneshsrinivasan96@gmail.com>
2022-06-26 11:18:37 +0530

    some modifications are added

--- src/org/ericsson/EricssonClass.java
+++ src/org/ericsson/EricssonClass.java
@@ -4,5 +4,15 @@ public class EricssonClass {
 private void employeeName() {
 	// TODO Auto-generated method stub
 System.out.println("employee name");
+
+
+}
+private void empolyeeId() {
+	// TODO Auto-generated method stub
+System.out.println("empolyee id");
+}
+private void empolyeeAddress() {
+	// TODO Auto-generated method stub
+System.out.println("employee address");
 }
 }
